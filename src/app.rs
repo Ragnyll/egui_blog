0308fde9627f6f68d6006a65dd64c6bf975ef402
ragnyll <ragnyll@gallowzhumour.dev>
2022-06-09 23:52:36 -0500

    ive got a side panel

--- src/app.rs
+++ src/app.rs
@@ -1,57 +1,62 @@
-pub struct TemplateApp {
-}
+use egui_extras::RetainedImage;
+
+pub struct TemplateApp {}
 
 impl Default for TemplateApp {
     fn default() -> Self {
-        Self { }
+        Self {}
     }
 }
 
 impl TemplateApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customized the look at feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
+        cc.egui_ctx.set_visuals(egui::Visuals::dark());
         Default::default()
     }
 }
 
-fn render_home(ui: &mut egui::Ui) {
-}
-
-fn render_blog(ui: &mut egui::Ui) {
-}
-
-fn render_resume(ui: &mut egui::Ui) {
-}
-
-fn show_menu(ui: &mut egui::Ui) {
-    use egui::menu;
-
-    menu::bar(ui, |ui| {
-        if ui.button("Home").clicked() {
-            println!("ay fuck you man");
-        }
-        if ui.button("Blog").clicked() {
-            println!("Gotta get that siracha shrimp");
-        }
-        if ui.button("Resume").clicked() {
-            println!("PINK GUY");
-        }
-    });
-}
-
 impl eframe::App for TemplateApp {
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        let image_path = include_bytes!("/home/ragnyll/dev/blog/assets/hacker_smaller.png");
+        let retained_image = RetainedImage::from_image_bytes("profile picture", image_path).unwrap();
+
+        egui::SidePanel::left("intro_panel")
+                .resizable(false)
+                .show(ctx, |ui| {
+                    ui.vertical_centered(|ui| {
+                        ui.add_space(16.00);
+                        ui.heading("Jake Gallow");
+                        ui.add_space(4.00);
+                    });
+
+                    ui.separator();
+                    retained_image.show(ui);
+                    ui.separator();
+                    ui.vertical_centered(|ui| {
+                        ui.add_space(10.00);
+                        ui.label("Software Engineer by day");
+                        ui.label("Bboy by night");
+                        ui.add_space(10.00);
+                        ui.separator();
+
+                        ui.add_space(12.00);
+                        ui.hyperlink_to("LinkedIn", "https://www.linkedin.com/in/jakegallow/");
+                        ui.add_space(2.00);
+                        ui.hyperlink_to("Github", "https://github.com/ragnyll");
+                        ui.add_space(2.00);
+                        ui.hyperlink_to("Blog", "https://blog.gallowzhumour.dev");
+                    });
+                });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                ui.add_space(16.00);
+                ui.heading("Curriculum Vitae");
+                ui.add_space(4.00);
+                ui.separator();
 
 
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                show_menu(ui)
             });
         });
     }
